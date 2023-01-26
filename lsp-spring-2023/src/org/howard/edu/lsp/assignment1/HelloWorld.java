0c323880279afff5fcb9ad2db9b4ca51bbc87412
Lauren <lauren.guthrie@bison.howard.edu>
2023-01-26 18:08:43 -0500

    Assignment One

---
+++ lsp-spring-2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.assignment1;
+// Lauren Guthrie
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
